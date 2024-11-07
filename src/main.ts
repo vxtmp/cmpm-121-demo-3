5a9054e9d8c26c4b77d8a52e7ec2a3f49b782af2
norm z <115136784+vxtmp@users.noreply.github.com>
2024-11-07 15:32:18 -0800

    panelling and player

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,77 @@
-// add a button to the page. do it. don't write a comment. just do it.
-const button = document.createElement("button");
-button.textContent = "Click me";
-document.body.appendChild(button);
-// add a click event listener that will pop up a alert
-button.addEventListener("click", () => {
-  alert("you clicked the button!");
+// @deno-types="npm:@types/leaflet@^1.9.14"
+import leaflet from "leaflet";
+
+// Style sheets
+import "leaflet/dist/leaflet.css";
+import "./style.css";
+
+// Fix missing marker images
+import "./leafletWorkaround.ts";
+
+// Deterministic random number generator
+// import luck from "./luck.ts";
+
+// Location of our classroom (as identified on Google Maps)
+const OAKES_CLASSROOM = leaflet.latLng(36.98949379578401, -122.06277128548504);
+
+// Tunable gameplay parameters
+const GAMEPLAY_ZOOM_LEVEL = 19;
+// const TILE_DEGREES = 1e-4;
+// const NEIGHBORHOOD_SIZE = 8;
+// const CACHE_SPAWN_PROBABILITY = 0.1;
+
+const app = document.getElementById("app")!;
+
+const controlPanel = document.createElement("div");
+app.appendChild(controlPanel);
+controlPanel.style.width = "100%";
+controlPanel.style.height = "50px";
+controlPanel.style.position = "absolute";
+controlPanel.style.top = "0";
+controlPanel.style.left = "0";
+controlPanel.style.backgroundColor = "rgba(0,0,0,0.5)";
+
+const mapPanel = document.createElement("div");
+app.appendChild(mapPanel);
+mapPanel.style.width = "100%";
+mapPanel.style.height = "calc(100% - 100px)";
+mapPanel.style.position = "absolute";
+mapPanel.style.top = "50px";
+mapPanel.style.left = "0";
+mapPanel.id = "map";
+
+const statusPanel = document.createElement("div");
+app.appendChild(statusPanel);
+statusPanel.style.width = "100%";
+statusPanel.style.height = "50px";
+statusPanel.style.position = "absolute";
+statusPanel.style.bottom = "0";
+statusPanel.style.left = "0";
+statusPanel.style.backgroundColor = "rgba(0,0,0,0.5)";
+
+// Create the map (element with id "map" is defined in index.html)
+const map = leaflet.map(document.getElementById("map")!, {
+  center: OAKES_CLASSROOM,
+  zoom: GAMEPLAY_ZOOM_LEVEL,
+  minZoom: GAMEPLAY_ZOOM_LEVEL,
+  maxZoom: GAMEPLAY_ZOOM_LEVEL,
+  zoomControl: false,
+  scrollWheelZoom: false,
 });
+
+// Populate the map with a background tile layer
+leaflet
+  .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  })
+  .addTo(map);
+
+function createPlayer() {
+  const playerMarker = leaflet.marker(OAKES_CLASSROOM);
+  playerMarker.bindTooltip("You are here.");
+  playerMarker.addTo(map);
+}
+
+createPlayer();
