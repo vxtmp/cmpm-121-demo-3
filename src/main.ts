d2519c79eaa45f29cb22181822d39b24ece8c11d
norm z <115136784+vxtmp@users.noreply.github.com>
2024-11-07 13:13:03 -0800

    L16 dialog button

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
-// todo
+// add a button to the page. do it. don't write a comment. just do it.
+const button = document.createElement("button");
+button.textContent = "Click me";
+document.body.appendChild(button);
+// add a click event listener that will pop up a alert
+button.addEventListener("click", () => {
+  alert("you clicked the button!");
+});
